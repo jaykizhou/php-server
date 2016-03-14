319fb2fdf3ce865588d2bd55734f43eb657534f1
jaykizhou <yiqianniyiye@126.com>
2016-03-14 17:58:51 +0800

    update

--- rio.c
+++ rio.c
@@ -3,7 +3,6 @@
 
 /*
  * 从描述符fd的当前文件位置最多传送n个字节到存储器位置usrbuf
- * 直接在存储器和文件之间传送数据，没有应用级缓冲
  */
 ssize_t rio_readn(int fd, void *usrbuf, size_t n)
 {
@@ -81,6 +80,8 @@ static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n)
         }
     }
 
+    // 比较调用所需的字节数n与内部缓冲区剩余的字节数rp->rio_cnt
+    // 取其中最小值
     cnt = n;
     if (rp->rio_cnt < n) {
         cnt = rp->rio_cnt;
@@ -90,4 +91,59 @@ static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n)
     rp->rio_cnt = cnt;
 
     return cnt;
+}
+
+/*
+ * 从读缓冲区取一行数据
+ */
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen)
+{
+    int n, rc;
+    char c, *bufp = usrbuf;
+
+    for (n = 1; n < maxlen; n++) {
+        if ((rc = rio_read(rp, &c, 1)) == 1) {
+            *bufp++ = c;
+            if (c == '\n') { // 读完了一行
+                break;
+            }
+        } else if (rc == 0) {
+            if (n == 1) {
+                return 0; // EOF,但没有读取任何数据
+            } else {
+                break; //EOF,但已经读取了一些数据
+            }
+        } else { // 出错
+            return -1;
+        }
+    }
+
+    *bufp = 0;
+    return n;
+}
+
+/*
+ * 读取n字节数据
+ */
+ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n)
+{
+    size_t nleft = n;
+    ssize_t nread;
+    char *bufp = usrbuf;
+
+    while(nleft > 0) {
+        if((nread = rio_read(rp, bufp, nleft)) < 0) {
+            if(errno == EINTR) { // 被信号处理程序中断返回
+                nread = 0;
+            } else {
+                return -1; // 读取数据出错
+            }
+        } else if(nread == 0) { // EOF
+            break; 
+        } 
+        nleft -= nread;
+        bufp += nread;
+    }
+
+    return (n - nleft);
 }
---
+++ rio.h
@@ -0,0 +1,7 @@
+#ifndef __RIO_H__
+#define __RIO_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
