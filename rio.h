c383bc7f9c9df1b8c46bd2c4cbfc2df9113baf38
jaykizhou <yiqianniyiye@126.com>
2016-03-15 18:01:35 +0800

    update

--- rio.h
+++ rio.h
@@ -14,4 +14,11 @@ typedef struct {
     char rio_buf[RIO_BUFSIZE];  // 内部缓冲区
 } rio_t;
 
+ssize_t rio_readn(int fd, void *usrbuf, size_t n);
+ssize_t rio_writen(int fd, void *usrbuf, size_t n);
+void rio_readinitb(rio_t *rp, int fd);
+static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n);
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen);
+ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n);
+
 #endif
